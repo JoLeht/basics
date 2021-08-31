e0eaa15f352a5f7244eb9b35238aaa27f5baed4c
JoLeht <joni.lehtonen@gmail.com>
2021-08-31 17:34:33 +0300

    Squashed commit of the following:
    commit 69c0f9882c06bd6a6329b3f2f8e51cb5904f89e2
    Author: JoLeht <joni.lehtonen@gmail.com>
    Date:   Tue Aug 31 17:29:05 2021 +0300
    
        Update mergesort.py
    
    commit b7af5f883f0800e8f267e7a7dc6d55ed9adb3ac9
    Author: JoLeht <89851919+JoLeht@users.noreply.github.com>
    Date:   Tue Aug 31 17:23:13 2021 +0300
    
        Update .gitignore
    
    commit 72957e4b520d2f4be4a09d63b0ed0378ac4ceb35
    Author: JoLeht <89851919+JoLeht@users.noreply.github.com>
    Date:   Tue Aug 31 17:22:24 2021 +0300
    
        Create .gitignore
    
    commit 5a5b6cc3ebf9bd9d1bd7487835d03afdf7a835cf
    Author: JoLeht <89851919+JoLeht@users.noreply.github.com>
    Date:   Tue Aug 31 17:07:53 2021 +0300
    
        Delete 1.in
    
    commit 01375d6559a60c612d370fcfa9386b6d325fbf47
    Author: JoLeht <89851919+JoLeht@users.noreply.github.com>
    Date:   Tue Aug 31 17:07:36 2021 +0300
    
        Delete 1.out
    
    commit dd2979f9609ce45e6739a9e07b895342efa62956
    Author: Mikko Luhtasaari <mikko.luhtasaari@tuni.fi>
    Date:   Tue May 12 13:11:49 2020 +0300
    
        Add file to test settings
    
    commit 4e6605f7f49a0c6632248c60145f2da46addc89a
    Author: Aku Niskanen <aku.niskanen@tuni.fi>
    Date:   Mon Oct 21 02:47:54 2019 -0700
    
        A simple test
    
    commit 07291091b01a983d256488357b263ddb087d0188
    Author: Aku Niskanen <aku.niskanen@tuni.fi>
    Date:   Mon Oct 21 02:14:09 2019 -0700
    
        Removed debug-prints

--- basics/mergesort.py
+++ basics/mergesort.py
@@ -8,13 +8,12 @@ def debug_print(debug_msg=None, **kwargs):
         print("{}: {}".format(key, value))
 
 
+<<<<<<< HEAD
 def mergesort(array):
-    debug_print(array=array)
     if len(array) <= 1:
         return array
 
     m = len(array) // 2
-    debug_print(m=m)
 
     left = mergesort(array[:m])
     right = mergesort(array[m:])
@@ -23,8 +22,6 @@ def mergesort(array):
 
 
 def merge(left, right):
-    debug_print(debug_msg="Merging...", left=left, right=right)
-
     merged = []
 
     while len(left) > 0 and len(right) > 0:
@@ -38,16 +35,14 @@ def merge(left, right):
     else:
         merged += right
 
-    debug_print(merged=merged)
     return merged
 
 
+=======
+>>>>>>> parent of dd8c56c (Merge-sort)
 if __name__ == "__main__":
     input_str = input("Enter numbers, separated by ',': ")
-
     input_list = input_str.split(",")
-    debug_print(input_list=input_list)
-
     value_list = []
     for x in input_list:
         try:
@@ -56,7 +51,9 @@ if __name__ == "__main__":
             print("Invalid input.")
             quit(1)
 
-    debug_print(value_list=value_list)
-
+<<<<<<< HEAD
     sorted_list = mergesort(value_list)
     print(sorted_list)
+=======
+    debug_print(value_list=value_list)
+>>>>>>> parent of dd8c56c (Merge-sort)
